ac74cfc2aab48d9c709969c0e7ce47ccd624f692
Miron Dima <mirontsov2098@mail.ru>
2023-03-28 21:03:10 +0300

    add change lesson27Hero

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,44 +1,40 @@
 const render = (data) => {
-	let arr = []
-
 	data.forEach(item => {
-
+		let arr = [];
 		const renderMovies = () => {
-			if(item.movies){
+			if (item.movies) {
 				item.movies.map(film => {
 					arr.push(`<span>${film}</span>`)
 				})
 			}
 		}
-
-			
-
-		const blockHero = document.querySelector('.block_heroes');
+		
+		renderMovies();
+		const row = document.querySelector('.row');
 		const div = document.createElement('div');
-		div.classList.add('block_card');
-		div.innerHTML = `<div class="card">` +
+		div.classList.add('block_heroes');
+		div.innerHTML = `<div class="col-xl-4 col-md-6 col-sm-12 mt-5 mb-3 hero">` +
+			`<div class="card">` +
 			`<div class="card-header">` +
 			`<span>${item.name}</span> </div>` +
-			`<img src= ${item.photo} class="card-img-top" alt= ${item.name ? item.name : "no data available"}>` +
+			`<img src= ${item.photo} class="card-img-top" alt=${item.name ? item.name : "no data available"}>` +
 			`<ul class="list-group list-group-flush">` +
-			`<li class="list-group-item"><span>Real name :</span>${item.realName ? item.realName : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Species :</span>${item.species ? item.species : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Citizenship :</span>${item.citizenship ? item.citizenship : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Birthday :</span>${item.birthDay ? item.birthDay : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Gender :</span>${item.gender ? item.gender : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Death day :</span>${item.deathDay ? item.deathDay : "no data available"}</li>` +
-			`<li class="list-group-item"><span>Status :</span>${item.status ? item.status : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Real name : </span>${item.realName ? item.realName : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Species : </span>${item.species ? item.species : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Citizenship : </span>${item.citizenship ? item.citizenship : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Birthday : </span>${item.birthDay ? item.birthDay : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Gender : </span>${item.gender ? item.gender : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Death day : </span>${item.deathDay ? item.deathDay : "no data available"}</li>` +
+			`<li class="list-group-item"><span>Status : </span>${item.status ? item.status : "no data available"}</li>` +
 			`</ul>` +
 			`<div class="card-body">` +
 			`<h5 class="card-title">Movies</h5>` +
-			`<p class="card-text">${arr.join('')}</p>`
-			renderMovies()
-
-
+			`<p class="card-text">${arr.join('')}</p>` +
+			`</div>`+
+			`</div>`
+			row.append(div)
 		console.log(div);
 	})
-
-
 }
 export { render }
 
